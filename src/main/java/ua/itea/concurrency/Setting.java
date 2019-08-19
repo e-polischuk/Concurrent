2801ef31b63dd270210ce372159f2f84399f4d9c
e-polischuk <z.polischuk@gmail.com>
2019-08-19 09:54:59 +0200

    dynamic settings

---
+++ src/main/java/ua/itea/concurrency/Setting.java
@@ -0,0 +1,54 @@
+package polischuk;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+public enum Setting {
+	STR_PROP ("strProp", "str"),
+	NUM_PROP ("intProp", 1),
+	BOOL_PROP ("boolProp", true);
+	
+	private static final Pattern INT_PAT = Pattern.compile("^-?\\d+$");
+	private static final List<String> BOOLS = Arrays.asList("true", "false", "yes", "no");
+	
+	private final String name;
+	private final Object defVal;
+	private Object value;
+	
+	private Setting(String name, Object defVal) {
+		this.name = name.trim().toUpperCase();
+		this.defVal = defVal;
+		this.value = defVal;
+	}
+	
+	synchronized static void setValues(Map<String, String> props) {
+		for(Setting s : Setting.values()) {
+			String propVal = props == null || props.get(s.name) == null ? "" : props.get(s.name);
+			Class<?> type = s.defVal.getClass();
+			if (type == Integer.class) {
+				propVal = propVal.trim();
+				s.value = INT_PAT.matcher(propVal).matches() ? Integer.parseInt(propVal) : s.defVal;
+			} else if (type == Boolean.class) {
+				propVal = propVal.trim().toLowerCase();
+				s.value = BOOLS.contains(propVal) ? "true".equals(propVal) || "yes".equals(propVal) : s.defVal;
+			} else {
+				s.value = propVal.trim().isEmpty() ? s.defVal : propVal;
+			}
+		}
+	}
+
+	public String asStr() {
+		return String.valueOf(value);
+	}
+	
+	public int asInt() {
+		return (int) value;
+	}
+	
+	public boolean asBool() {
+		return (boolean) value;
+	}
+	
+}
